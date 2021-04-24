4080f4a99ce563050a2447c2124d2cf05998f64b
Rocketman <759094438@qq.com>
2021-04-24 15:23:02 +0800

    first window in glfw

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,68 @@
+/*
+    Rocketman
+    2021-04-24
+    test main
+*/
+
+extern crate gl;
+extern crate gl_loader;
+extern crate glfw;
+use self::glfw::{Context, Key, Action};
+
+use std::sync::mpsc::Receiver;
+
+const WINDOW_TITLE: &'static str = "00.Base Code";
+const WINDOW_WIDTH: u32 = 800;
+const WINDOW_HEIGHT: u32 = 600;
+
+fn process_events(window: &mut glfw::Window, events: &Receiver<(f64, glfw::WindowEvent)>) {
+    for (_, event) in glfw::flush_messages(events) {
+        match event {
+            glfw::WindowEvent::FramebufferSize(width, height) => {
+                // make sure the viewport matches the new window dimensions; note that width and
+                // height will be significantly larger than specified on retina displays.
+                unsafe { gl::Viewport(0, 0, width, height) }
+            }
+            glfw::WindowEvent::Key(Key::Escape, _, Action::Press, _) => window.set_should_close(true),
+            _ => {}
+        }
+    }
+}
+
 fn main() {
     println!("Hello, world!");
+
+    let mut glfw = glfw::init(glfw::FAIL_ON_ERRORS).unwrap();
+    glfw.window_hint(glfw::WindowHint::ContextVersion(3, 3));
+    glfw.window_hint(glfw::WindowHint::OpenGlProfile(glfw::OpenGlProfileHint::Core));
+    #[cfg(target_os = "macos")]
+    glfw.window_hint(glfw::WindowHint::OpenGlForwardCompat(true));
+
+    // glfw window creation
+    let (mut window, events) = glfw.create_window(WINDOW_WIDTH, WINDOW_HEIGHT, WINDOW_TITLE, glfw::WindowMode::Windowed)
+        .expect("Failed to create GLFW window");
+
+    window.make_current();
+    window.set_key_polling(true);
+    window.set_framebuffer_size_polling(true);
+
+    // load opengl
+    gl_loader::init_gl();
+    gl::load_with(|symbol| gl_loader::get_proc_address(symbol) as *const _);
+
+    // render loop
+    while !window.should_close() {
+        // events
+        process_events(&mut window, &events);
+
+        // render
+        unsafe {
+            gl::ClearColor(0.2, 0.3, 0.3, 1.0);
+            gl::Clear(gl::COLOR_BUFFER_BIT);
+        }
+
+        // glfw: swap buffers and poll IO events
+        window.swap_buffers();
+        glfw.poll_events();
+    }
 }
