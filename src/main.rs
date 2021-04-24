c5e9f61214609591afc1d25646e2ab8e2f1ccfa3
Rocketman <759094438@qq.com>
2021-04-24 19:24:28 +0800

    update window creation program

--- src/main.rs
+++ src/main.rs
@@ -6,63 +6,78 @@
 
 extern crate gl;
 extern crate gl_loader;
-extern crate glfw;
-use self::glfw::{Context, Key, Action};
+//use vulkano::instance::Instance;
+//use vulkano::instance::InstanceExtensions;
+//use vulkano::instance::PhysicalDevice;
 
-use std::sync::mpsc::Receiver;
+//use winit::event::{Event, VirtualKeyCode, ElementState, KeyboardInput, WindowEvent};
+//use winit::event_loop::{EventLoop, ControlFlow};
+//use winit::window::WindowBuilder;
 
-const WINDOW_TITLE: &'static str = "00.Base Code";
+use glutin::event::{Event, WindowEvent, VirtualKeyCode, ElementState, KeyboardInput};
+use glutin::event_loop::{ControlFlow, EventLoop};
+use glutin::window::WindowBuilder;
+use glutin::ContextBuilder;
+
+use std::ffi::CStr;
+
+const WINDOW_TITLE: &'static str = "Rocket";
 const WINDOW_WIDTH: u32 = 800;
 const WINDOW_HEIGHT: u32 = 600;
-
-fn process_events(window: &mut glfw::Window, events: &Receiver<(f64, glfw::WindowEvent)>) {
-    for (_, event) in glfw::flush_messages(events) {
-        match event {
-            glfw::WindowEvent::FramebufferSize(width, height) => {
-                // make sure the viewport matches the new window dimensions; note that width and
-                // height will be significantly larger than specified on retina displays.
-                unsafe { gl::Viewport(0, 0, width, height) }
-            }
-            glfw::WindowEvent::Key(Key::Escape, _, Action::Press, _) => window.set_should_close(true),
-            _ => {}
-        }
-    }
-}
 
 fn main() {
     println!("Hello, world!");
 
-    let mut glfw = glfw::init(glfw::FAIL_ON_ERRORS).unwrap();
-    glfw.window_hint(glfw::WindowHint::ContextVersion(3, 3));
-    glfw.window_hint(glfw::WindowHint::OpenGlProfile(glfw::OpenGlProfileHint::Core));
-    #[cfg(target_os = "macos")]
-    glfw.window_hint(glfw::WindowHint::OpenGlForwardCompat(true));
+    let event_loop = glutin::event_loop::EventLoop::new();
+    let window_builder = glutin::window::WindowBuilder::new()
+        .with_title(WINDOW_TITLE)
+        .with_inner_size(glutin::dpi::LogicalSize::new(WINDOW_WIDTH, WINDOW_HEIGHT));
+    let windowed_context = glutin::ContextBuilder::new()
+        .build_windowed(window_builder, &event_loop)
+        .unwrap();
 
-    // glfw window creation
-    let (mut window, events) = glfw.create_window(WINDOW_WIDTH, WINDOW_HEIGHT, WINDOW_TITLE, glfw::WindowMode::Windowed)
-        .expect("Failed to create GLFW window");
+    let windowed_context = unsafe { windowed_context.make_current().unwrap() };
 
-    window.make_current();
-    window.set_key_polling(true);
-    window.set_framebuffer_size_polling(true);
+    println!("Pixel format of the window's GL context: {:?}", windowed_context.get_pixel_format());
 
     // load opengl
     gl_loader::init_gl();
     gl::load_with(|symbol| gl_loader::get_proc_address(symbol) as *const _);
 
+    let version = unsafe {
+        let data = CStr::from_ptr(gl::GetString(gl::VERSION) as *const _).to_bytes().to_vec();
+        String::from_utf8(data).unwrap()
+    };
+
+    println!("OpenGL version {}", version);
+    
     // render loop
-    while !window.should_close() {
-        // events
-        process_events(&mut window, &events);
+    event_loop.run(move |event, _, control_flow| {
+        println!("{:?}", event);
+        *control_flow = ControlFlow::Wait;
 
-        // render
-        unsafe {
-            gl::ClearColor(0.2, 0.3, 0.3, 1.0);
-            gl::Clear(gl::COLOR_BUFFER_BIT);
+        match event {
+            Event::LoopDestroyed => {
+                return;
+            },
+            Event::WindowEvent { event, .. } => match event {
+                WindowEvent::Resized(physical_size) => windowed_context.resize(physical_size),
+                WindowEvent::CloseRequested => *control_flow = ControlFlow::Exit,
+                _ => (),
+            },
+            Event::RedrawRequested(_) => {
+                unsafe {
+                    gl::ClearColor(0.2, 0.3, 0.3, 1.0);
+                    gl::Clear(gl::COLOR_BUFFER_BIT);
+                }
+                //gl.draw_frame([1.0, 0.5, 0.7, 1.0]);
+                windowed_context.swap_buffers().unwrap();
+            }
+            _ => (),
         }
+    });
 
-        // glfw: swap buffers and poll IO events
-        window.swap_buffers();
-        glfw.poll_events();
-    }
+    gl_loader::end_gl();
+
+    println!("End");
 }
